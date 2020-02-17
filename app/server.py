fdccb363c461f10dff003c53ea72c045e012ca30
Gina Huh <jhuh95@utexas.edu>
2020-02-16 20:31:50 -0600

    wtf

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/qr11glez1ofngnb/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['cows','longhorns']
 path = Path(__file__).parent
 
 app = Starlette()
